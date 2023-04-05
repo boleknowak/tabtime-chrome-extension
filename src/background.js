552726f751e443f7763376196a43a1031d6c6ebe
boleknowak <boleknowak@o2.pl>
2023-04-05 14:53:40 +0200

    updated code format

--- src/background.js
+++ src/background.js
@@ -5,6 +5,9 @@ if (chrome.tabs) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
       if (currentTab) {
+        if (typeof currentTab.url === 'undefined') return;
+        if (currentTab.url.startsWith('chrome://')) return;
+
         const url = new URL(currentTab.url || '');
         const data = {
           title: currentTab.title,
