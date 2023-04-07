16e2690e9262f78477328cccbf06747e480d255d
boleknowak <boleknowak@o2.pl>
2023-04-07 14:46:47 +0200

    added view for empty token state

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const API_URL = 'http://localhost:3003/v1';
 export default function App() {
   const [token, setToken] = useState<string | null>(null);
   const [isSavingKey, setIsSavingKey] = useState(false);
+  const [isSavedKey, setIsSavedKey] = useState(false);
   const [isEditingKeyState, setIsEditingKeyState] = useState(false);
   const [isLoadingData, setIsLoadingData] = useState(true);
   const [time, setTime] = useState(null);
@@ -85,6 +86,7 @@ export default function App() {
     chrome.storage.sync.get(['token', 'selectedTimeRange'], (result) => {
       setToken(result.token);
       setTime(result.selectedTimeRange || '24h');
+      setIsSavedKey(!!result.token);
     });
   }, []);
 
@@ -94,6 +96,12 @@ export default function App() {
     setIsSavingKey(true);
     chrome.storage.sync.set({ token }, () => {
       setIsSavingKey(false);
+      setIsSavedKey(true);
+      setIsEditingKeyState(false);
+
+      if (token === null || token === '') {
+        setIsSavedKey(false);
+      }
     });
   };
 
@@ -105,85 +113,97 @@ export default function App() {
     chrome.storage.sync.set({ selectedTimeRange: range }, () => {});
   };
 
-  // TODO: add view when user not provided token
   return (
     <div className="w-[400px] p-4 text-center">
       <div className="mb-4 text-left">
         <h1 className="text-2xl font-bold">TabTime</h1>
         <div className="text-xs font-medium">{message}</div>
       </div>
-      <div className="mt-4 border-2 border-black p-4">
-        {!isLoadingData && (
-          <div>
-            <div className="text-lg font-bold">{currentStats.time}</div>
-            <div className="leading-4">{currentStats.origin}</div>
+      {isSavedKey && (
+        <div>
+          <div className="mt-4 border-2 border-black p-4">
+            {!isLoadingData && (
+              <div>
+                <div className="text-lg font-bold">{currentStats.time}</div>
+                <div className="leading-4">{currentStats.origin}</div>
+              </div>
+            )}
+            {isLoadingData && (
+              <div className="space-y-2">
+                <div className="mx-auto h-6 w-48 animate-pulse rounded bg-gray-100"></div>
+                <div className="mx-auto h-3 w-28 animate-pulse rounded bg-gray-100"></div>
+              </div>
+            )}
           </div>
-        )}
-        {isLoadingData && (
-          <div className="space-y-2">
-            <div className="mx-auto h-6 w-48 animate-pulse rounded bg-gray-100"></div>
-            <div className="mx-auto h-3 w-28 animate-pulse rounded bg-gray-100"></div>
+          <div className="mt-2 flex flex-row items-center justify-between">
+            <div className="flex flex-row items-center space-x-2">
+              <button
+                type="button"
+                onClick={() => setIsEditingKeyState(!isEditingKeyState)}
+                className={`border-2 border-black px-2 font-bold uppercase ${
+                  isEditingKeyState && 'bg-gray-600 text-white'
+                }`}
+              >
+                Edit key
+              </button>
+            </div>
+            <div className="flex flex-row items-center space-x-2">
+              <button
+                type="button"
+                onClick={() => setRange('24h')}
+                className={`border-2 border-black px-1 font-bold ${
+                  time === '24h' && 'bg-gray-600 text-white'
+                }`}
+              >
+                24h
+              </button>
+              <button
+                type="button"
+                onClick={() => setRange('7d')}
+                className={`border-2 border-black px-1 font-bold ${
+                  time === '7d' && 'bg-gray-600 text-white'
+                }`}
+              >
+                7d
+              </button>
+              <button
+                type="button"
+                onClick={() => setRange('all')}
+                className={`border-2 border-black px-1 font-bold ${
+                  time === 'all' && 'bg-gray-600 text-white'
+                }`}
+              >
+                All
+              </button>
+            </div>
           </div>
-        )}
-      </div>
-      <div className="mt-2 flex flex-row items-center justify-between">
-        <div className="flex flex-row items-center space-x-2">
-          <button
-            type="button"
-            onClick={() => setIsEditingKeyState(!isEditingKeyState)}
-            className={`border-2 border-black px-2 font-bold uppercase ${
-              isEditingKeyState && 'bg-gray-600 text-white'
-            }`}
-          >
-            Edit key
-          </button>
         </div>
-        <div className="flex flex-row items-center space-x-2">
-          <button
-            type="button"
-            onClick={() => setRange('24h')}
-            className={`border-2 border-black px-1 font-bold ${
-              time === '24h' && 'bg-gray-600 text-white'
-            }`}
-          >
-            24h
-          </button>
-          <button
-            type="button"
-            onClick={() => setRange('7d')}
-            className={`border-2 border-black px-1 font-bold ${
-              time === '7d' && 'bg-gray-600 text-white'
-            }`}
-          >
-            7d
-          </button>
-          <button
-            type="button"
-            onClick={() => setRange('all')}
-            className={`border-2 border-black px-1 font-bold ${
-              time === 'all' && 'bg-gray-600 text-white'
-            }`}
-          >
-            All
-          </button>
-        </div>
-      </div>
-      {isEditingKeyState && (
-        <div className="mt-4 flex flex-grow flex-row space-x-4">
-          <input
-            type="password"
-            className="w-full border-2 border-black px-2 py-1 text-center text-sm focus:border-black focus:outline-none"
-            placeholder="Enter your key"
-            value={token}
-            onChange={(e) => setToken(e.target.value)}
-          />
-          <button
-            onClick={() => setUserToken()}
-            disabled={isSavingKey}
-            className="block w-48 bg-black px-2 py-1 font-bold uppercase text-white"
-          >
-            Save key
-          </button>
+      )}
+      {(isEditingKeyState || !isSavedKey) && (
+        <div>
+          <div className="mt-4 flex flex-grow flex-row space-x-4">
+            <input
+              type="password"
+              className="w-full border-2 border-black px-2 py-1 text-center text-sm focus:border-black focus:outline-none"
+              placeholder="Enter your key"
+              value={token}
+              onChange={(e) => setToken(e.target.value)}
+            />
+            <button
+              onClick={() => setUserToken()}
+              disabled={isSavingKey}
+              className={`block w-48 bg-black px-2 py-1 font-bold uppercase text-white ${
+                isSavedKey && 'bg-opacity-50'
+              }'}`}
+            >
+              Save key
+            </button>
+          </div>
+          <div className="mt-1 text-left text-xs text-gray-600">
+            <a href="" target="_blank" className="hover:underline">
+              Click here to get a token
+            </a>
+          </div>
         </div>
       )}
       <div className="mt-4 space-x-1 text-left text-xs text-gray-500">
